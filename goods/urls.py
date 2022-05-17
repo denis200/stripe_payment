501ca1fef8a1581583d57888728c3c76fd8a2cee
Denis Komarkovu <soundze@mail.ru>
2022-05-17 13:56:39 +0300

    Feat: Add payment in ItemView, add Success adn Cansel Views

--- goods/urls.py
+++ goods/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from goods.views import index
+from goods.views import ItemDetailView, cancel_view, item_view, success_view
 
 
 urlpatterns = [
-    path('',index)
+    path('item/<int:pk>',item_view),
+    path('detail/<int:pk>/', ItemDetailView.as_view(), name = 'detail'),
+    path('success/',success_view()),
+    path('cansel/',cancel_view()),
+
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import ListView, DetailView, DeleteView
+import stripe
 
-def index(request):
-    return render(request,'index.html')
+from goods.models import Item
+
+def item_view(request,pk):
+    session = stripe.checkout.Session.create(
+    line_items=[{
+      'price_data': {
+        'currency': 'usd',
+        'product_data': {
+          'name': 'T-shirt',
+        },
+        'unit_amount': 2000,
+      },
+      'quantity': 1,
+    }],
+    mode='payment',
+    success_url='https://127.0.0.1:8080/success',
+    cancel_url='https://127.0.0.1:8080/cancel',
+  )
+    return redirect(session.url)
+
+
+def success_view(request):
+    render(request,'success.html')
+
+
+def cancel_view(request):
+    render(request,'cansel.html')
+
+
+class ItemDetailView(DetailView):
+    queryset = Item.objects.all()
+    template_name = 'item.html'
